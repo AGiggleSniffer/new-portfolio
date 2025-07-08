90f64cda8c79079368c8a6a44e02cb1231a0e9e6
AGiggleSniffer <97860788+AGiggleSniffer@users.noreply.github.com>
2025-07-08 03:43:38 -0700

    start up

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Hero = () => {
 	return (
 		<div className="w-svw h-svh py-48 px-24">
-			<h1 className="text-9xl">
+			<h1 className="text-8xl">
 				Hello I&apos;m Chris<span>👋</span>
 			</h1>
-			<h2 className="text-xl px-3 py-6">
+			<h2 className="text px-3 py-6">
 				A software engineer who likes to make immersive environments and
 				enjoys seeing people excited by software
 			</h2>
--- src/app/components/HeroCanvas/index.tsx
+++ src/app/components/HeroCanvas/index.tsx
@@ -1,20 +1,23 @@
 "use client";
 import React from "react";
 import { Canvas } from "@react-three/fiber";
+// import { Plane, PlaneGeometry, Vector3 } from "three";
 
 const HeroCanvas = () => {
+	// const planeRef = useRef();
+	// const myPlane = new Plane(new Vector3(0, 1, 0), 0);
 	return (
 		<div className="absolute top-0 -z-10 h-svh w-svw">
-			<div className="absolute size-64 top-1/2 right-1/4">
-				<Canvas>
-					<ambientLight intensity={0.3} />
-					<directionalLight />
-					<mesh>
-						<sphereGeometry args={[2]}  />
-						<meshPhongMaterial color={"red"} />
-					</mesh>
-				</Canvas>
-			</div>
+			<Canvas>
+				<ambientLight intensity={0.3} />
+				<directionalLight />
+				<mesh>
+					<sphereGeometry args={[2]} />
+					<meshPhongMaterial wireframe color={"red"} />
+				</mesh>
+				<mesh></mesh>
+				{/* <planeHelper args={[myPlane]} /> */}
+			</Canvas>
 		</div>
 	);
 };
